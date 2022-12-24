9fbf5bf57cbf3e2474ffe5b2bad65753e0dadbf8
stefan94poz <stefan.s.webdev@gmail.com>
2022-12-24 17:27:16 +0100

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { ProjectsModule } from './projects/projects.module';
+import { CategoriesModule } from './categories/categories.module';
+
+@Module({
+  controllers: [],
+  providers: [PrismaModule],
+  imports: [UsersModule, PostsModule, ProjectsModule, CategoriesModule],
+})
+export class AppModule {}
